ccf81f0df72ec7f1f2f0884ae443fb98dda20e97
Matt Davis <jiffyclub@gmail.com>
2016-04-27 19:12:34 -0700

    final code from tutorial

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask('demo')
 
 @app.route('/hello')
 def hello():
-    return 'Hello!'
+    name = request.args.get('name')
+    greeting = request.args.get('greeting')
+    return '{greeting}, {name}!'.format(
+        greeting=greeting, name=name)
+
+@app.route('/bye')
+def bye():
+    return 'Goodbye!'
+
+@app.route('/')
+def main():
+    return "You're at the beginning!"
+
+@app.route('/wiki/<topic>')
+def wiki(topic):
+    return topic
+
+@app.route('/welcome')
+def welcome():
+    name = request.args.get('name')
+    return render_template(
+        'welcome.html', name=name)
 
 app.run(debug=True)
